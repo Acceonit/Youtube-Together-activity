1eb043bef5e191c7d41a77d69799b6f39875bf0a
Tommy <76491068+Acceonit@users.noreply.github.com>
2021-10-14 13:03:24 +0100

    YouTube together

--- youtubetogether.py
+++ youtubetogether.py
@@ -1,8 +1,17 @@
-from discordTogether import DiscordTogether    #in import lines
+from discord.ext import commands
+from discordTogether import DiscordTogether    
 
-togetherControl =  DiscordTogether(client)   #above on_ready message
+togetherControl =  DiscordTogether(client)   
 
+@client.event
+async def on_ready():
+    print(f"Bot logged into {client.user}.")
+    
+    
 @client.command()
 async def start(ctx):
     link = await togetherControl.create_link(ctx.author.voice.channel.id, 'youtube')
     await ctx.send(f"Click the blue link!\n{link}"
+                   
+             
+client.run("BOT_TOKEN_HERE")
